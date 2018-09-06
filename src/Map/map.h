1a1aa6df20f43931f1df378be6ad0bd1eb3886f5
yoshiyoshi1 <ysdsiy0124@gmail.com>
2018-09-06 14:44:41 +0900

    09.06 GoogleDriveUP
    Change :
    　main, STitle, SGame, RouteSearch
    　map, CMesh, Helper
    Add :
    　human, model&texture&csv

--- src/Map/map.h
+++ src/Map/map.h
@@ -2,29 +2,32 @@
 #define __map_h__
 
 class map {
-private:
-	const int		MAPWIDTH = 80;
-	const int		MAPHEIGHT = 80;
-
-	void LoadXFile(void);
 public:
+	static const int SIZE_X = 80;
+	static const int SIZE_Y = 80;
 
-	void LoadMap(void);
-	void DrawMap(CVector3 _pos);
+	void LoadXFile(void);						// メッシュデータ読み込み
 
-	// 地形
-	FILE* t_fp;
-	int t_MapData[80][80];
+	void LoadMap(void);						// mapの読み込み
+	void DrawMap(CVector3 _pos);		// mapの描画
 
-	// 建物
-	FILE* b_fp;
-	int b_MapData[80][80];
-	
-	CMesh m_meshLand;
-	CMesh m_meshRoad;
-	CMesh m_meshHouse[9];
+										// 地形
+	FILE* t_fp;									// 地形fileポインタ
+	int t_MapData[80][80];					// 地形mapデータ
 
-	CMatrix m[80][80];
+											// 建物
+	FILE* b_fp;									// 建物fileポインタ
+	int b_MapData[80][80];					// 建物mapデータ
+
+private:
+	CMesh m_meshLand;					// 地形メッシュ
+	CMesh m_meshRoad;					// 道路メッシュ
+	CMesh m_meshSee;						// 海メッシュ
+	CMesh m_meshSand;					// 砂メッシュ
+	CMesh m_meshHouse[20];			// 建物メッシュ
+
+	CMatrix m[80][80];						// map行列
+	CMatrix mSee[100][100];					// map行列(海用)
 
 };
 #endif
